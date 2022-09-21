41fa1476afde2a8440823fbb3c08bb77cac48fc2
Ziham Zuhair <=>
2022-09-21 19:20:33 +0530

    git ignore file and response file added

---
+++ src/main/java/com/ds/server/models/OrderResponse.java
@@ -0,0 +1,37 @@
+package com.ds.server.models;
+
+public class OrderResponse {
+    String Id;
+    Boolean status;
+
+    public String getId() {
+        return Id;
+    }
+
+    double AvailableQuantity;
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public double getAvailableQuantity() {
+        return AvailableQuantity;
+    }
+
+    public void setAvailableQuantity(double availableQuantity) {
+        AvailableQuantity = availableQuantity;
+    }
+
+
+
+
+
+}
